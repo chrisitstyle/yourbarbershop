2a9c212f1f73c5584ff24b2e7fa1678d0ab1e7fa
chrisitstyle <infernos1244@o2.pl>
2024-05-26 17:17:31 +0200

    Add supabaseApi file and env_example file

---
+++ src/api/supabaseApi.js
@@ -0,0 +1,3 @@
+export const projectURL = process.env.REACT_APP_SUPABASE_PROJECTURL;
+export const publicApiKey = process.env.REACT_APP_SUPABASE_PUBLICAPIKEY;
+export const CDNURL = process.env.REACT_APP_SUPABASE_CDNURL;
