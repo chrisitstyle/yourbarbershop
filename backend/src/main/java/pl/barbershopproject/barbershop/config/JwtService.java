fb073c6f2008894ef11da65637b2ce872a573679
Krzysztof Podjacki <43932836+chrisitstyle@users.noreply.github.com>
2026-06-07 11:06:59 +0200

    Chore/update jjwt 0.13.0 (#7)
    * chore(deps): update jjwt to 0.13.0 and migrate JWT API
    
    * chore(deps): update springdoc and Jackson dependencies

--- backend/src/main/java/pl/barbershopproject/barbershop/config/JwtService.java
+++ backend/src/main/java/pl/barbershopproject/barbershop/config/JwtService.java
@@ -2,7 +2,6 @@ package pl.barbershopproject.barbershop.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,41 +19,44 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
+
     @Value("${JWT_SECRET_KEY}")
-    private String SECRET_KEY;
+    private String secretKey;
 
     private SecretKey getSignKey() {
-        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET_KEY));
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
     }
 
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> extraClaims = new HashMap<>();
+
         if (userDetails instanceof User user) {
             extraClaims.put("role", user.getRole().toString());
             extraClaims.put("id", user.getIdUser());
         }
+
         return generateToken(extraClaims, userDetails);
     }
 
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         final Instant now = Instant.now();
         final Instant expiry = now.plus(8, ChronoUnit.HOURS);
-        return Jwts
-                .builder()
-                .setClaims(extraClaims)
-                .setSubject(userDetails.getUsername())
-                .setIssuedAt(Date.from(now))
-                .setExpiration(Date.from(expiry))
-                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+
+        return Jwts.builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(expiry))
+                .signWith(getSignKey(), Jwts.SIG.HS256)
                 .compact();
     }
 
     private Claims extractAllClaims(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(getSignKey())
+        return Jwts.parser()
+                .verifyWith(getSignKey())
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
