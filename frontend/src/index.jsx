cd17a69e42a7b3836227382ffeaeba3ecad6bd01
chrisitstyle <43932836+chrisitstyle@users.noreply.github.com>
2026-07-10 22:17:11 +0200

    feat: improve dark theme

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 import "./index.css";
-import App from "./App";
 import "./api/i18n";
+
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
